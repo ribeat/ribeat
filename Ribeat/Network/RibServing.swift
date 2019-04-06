46ecfb22479bff592ad8e021494e1be81011f249
Alin Brindusescu <alinoz77@gmail.com>
2019-04-07 00:13:24 +0300

    Added order POST and PUT

--- Ribeat/Network/RibServing.swift
+++ Ribeat/Network/RibServing.swift
@@ -38,6 +38,10 @@ class RibServing {
             return self.newOrder(bodyData: Data(bytes: request.body, count: request.body.count))
         }
         
+        server.PUT["/api/v1/orders"] = { request in
+            return self.updateOrder(bodyData: Data(bytes: request.body, count: request.body.count))
+        }
+        
         try! server.start()
     }
     
@@ -90,8 +94,37 @@ class RibServing {
         }
     }
     
-    //curl -v -k -H "Content-Type: aapplication/json; charset=UTF-8" -X POST -d '{"table": "1", "orderItems": []}' http://192.168.86.153:8080/api/v1/orders
+    func getProduct(productId: Int32) -> Product? {
+        let productFetch = NSFetchRequest<NSFetchRequestResult>(entityName: "Product")
+        let predicate = NSPredicate(format: "pId = %d", argumentArray: [productId])
+        productFetch.predicate = predicate
+        
+        do {
+            let products = try persistentContainer.viewContext.fetch(productFetch) as! [Product]
+            if products.count > 0 {
+                return products[0]
+            }
+        } catch {
+        }
+        return nil
+    }
     
+    func getOrder(orderId: String) -> Order? {
+        let ordersFetch = NSFetchRequest<NSFetchRequestResult>(entityName: "Order")
+        let predicate = NSPredicate(format: "orderId = %@", argumentArray: [orderId])
+        ordersFetch.predicate = predicate
+        do {
+            let orders = try persistentContainer.viewContext.fetch(ordersFetch) as! [Order]
+            if orders.count > 0 {
+                return orders[0]
+            }
+        } catch {
+        }
+        
+        return nil
+    }
+    
+    //curl -v -k -H "Content-Type: application/json; charset=UTF-8" -X POST -d '{"table": "1", "orderItems": [{"productId": 2, "quantity": 2, "baypassDeliveryOrder": false}, {"productId": 10, "quantity": 3, "baypassDeliveryOrder": true}]}' http://192.168.86.153:8080/api/v1/orders
     func newOrder(bodyData data: Data) -> HttpResponse {
         do {
             let json = try JSONSerialization.jsonObject(with: data, options: [])
@@ -101,11 +134,58 @@ class RibServing {
                     order.table = orderTable
                     let orderDate = Date()
                     order.datetime = orderDate
-                    order.orderReady = false
+                    order.orderReady = 0
                     order.orderId = "\(orderDate.timeIntervalSince1970)-\(orderTable)"
+                    if let oItems = orderDict["orderItems"] as? [[String: Any]] {
+                        for oItem in oItems {
+                            if let itemProductId = oItem["productId"] as? Int32 {
+                                if let product = getProduct(productId: itemProductId) {
+                                    let orderItem = OrderItem(context: persistentContainer.viewContext)
+                                    orderItem.oiId = "\(order.orderId!)-\(product.pId)"
+                                    orderItem.baypassDeliveryOrder = oItem["baypassDeliveryOrder"] as? Bool ?? false
+                                    orderItem.quantity = oItem["quantity"] as? Int16 ?? 1
+                                    orderItem.product = product
+                                    order.addToItems(orderItem)
+                                }
+                            }
+                        }
+                    }
                     try persistentContainer.viewContext.save()
                     let responseBody = "{'orderId': '\(order.orderId!)'}"
                     return HttpResponse.ok(.text(responseBody))
+                }
+            }
+        } catch {
+        }
+        return HttpResponse.internalServerError
+    }
+    
+    
+    
+    //curl -v -k -H "Content-Type: application/json; charset=UTF-8" -X PUT -d '{"table": "1", "orderId":"1554582345.864249-1", "orderReady": "1", "orderItems": [{"productId": 2, "quantity": 2, "baypassDeliveryOrder": false}, {"productId": 10, "quantity": 3, "baypassDeliveryOrder": true}]}' http://192.168.86.153:8080/api/v1/orders
+    func updateOrder(bodyData data: Data) -> HttpResponse {
+        do {
+            let json = try JSONSerialization.jsonObject(with: data, options: [])
+            if let orderDict = json as? [String: Any] {
+                if let orderId = orderDict["orderId"] as? String {
+                    if let order = getOrder(orderId: orderId) {
+                        
+                        if let orderReady = orderDict["orderReady"] as? String {
+                            order.orderReady = Int16(orderReady) ?? 0
+                        }
+                        
+                        if let paymentType = orderDict["paymentType"] as? String {
+                            order.paymentType = paymentType
+                        }
+                        
+                        if let companyNmber = orderDict["companyNmber"] as? String {
+                            order.companyNmber = companyNmber
+                        }
+                        
+                        try persistentContainer.viewContext.save()
+                        let responseBody = "{'orderId': '\(order.orderId!)'}"
+                        return HttpResponse.ok(.text(responseBody))
+                    }
                 }
             }
         } catch {
