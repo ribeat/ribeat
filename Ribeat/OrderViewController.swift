c94eb804f26d43afc5de493de2932bed1987f224
Emanuel Covaci <covaciemanuel98@gmail.com>
2019-04-06 22:51:47 +0300

    TableView Cell in Order Screen

--- Ribeat/OrderViewController.swift
+++ Ribeat/OrderViewController.swift
@@ -8,11 +8,29 @@
 
 import UIKit
 
-class OrderViewController: UIViewController {
+class OrderViewController: UIViewController,  UITableViewDelegate, UITableViewDataSource {
+    
     var table: String = ""
+    var images =  [UIImage(named: "p_bbq"), UIImage(named: "p_beef"), UIImage(named: "p_caesar"), UIImage(named: "pc_fish"), UIImage(named: "p_pineappleGinger")]
+    
+
+
+    
+    
+    @IBOutlet var productTable: UITableView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        productTable.delegate = self
+        productTable.dataSource = self
+        productTable.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
+   //     productTable.separatorStyle = .none
+        productTable.rowHeight = 44.0
+        productTable.separatorColor = UIColor.white
+        
+    
         
     }
     
@@ -20,7 +38,22 @@ class OrderViewController: UIViewController {
         title = table
     }
     
-
-   
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = productTable.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+        cell.backgroundColor = UIColor.white
+        print(indexPath)
+        cell.textLabel?.text = "Hello World"
+        cell.imageView?.image = images.randomElement() ?? images[0]
+        
+        return cell
+    }
+    
 
 }
+
+
+
